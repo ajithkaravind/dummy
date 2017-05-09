24629b8be8b1478f6fbd34c1c42b17faad4e02fb
Ajith <ajithkaravind@gmail.com>
2017-05-09 18:42:42 +0530

    Initial commit

---
+++ Dummy project/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Dummy project
+//
+//  Created by Ajith on 5/9/17.
+//  Copyright © 2017 sinergiasinergia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
